a85c7008d04432ce87f89fab560dd5edcfd70cc9
Vaibhav Sharma <vaibhav@umn.edu>
2017-11-29 13:28:46 -0600

    making function name be optional since we may want to mutate the entire binary

--- make-mutants.pl
+++ make-mutants.pl
@@ -7,17 +7,18 @@ die "Usage: make-mutants.pl <absolute path to binary> [<function name>] [<debug>
 my ($binary,$fn_name,$debug);
 my $length = @ARGV;
 $fn_name = "";
-if($length == 2) {
-    ($binary, $fn_name) = @ARGV;
-} elsif($length == 3) {
-    ($binary, $fn_name, $debug) = @ARGV; 
-}
+
 my $output;
 while(1) {
-  if($debug==1 || $debug=="true") {
-    $output = `REPLACEINSN_DEBUG=1 ./ReplaceInsn $binary $fn_name`;
-  } else {
+  if($length == 1) { 
+      ($binary) = @ARGV; 
+      $output = `./ReplaceInsn $binary`;
+  } elsif($length == 2) {
+      ($binary, $fn_name) = @ARGV;
       $output = `./ReplaceInsn $binary $fn_name`;
+  } elsif($length == 3) {
+      ($binary, $fn_name, $debug) = @ARGV; 
+      $output = `./ReplaceInsn $binary $fn_name $debug`;
   }
   print $output;
   if(index($output, "No mutations found") != -1) { last; }
