555c63d72d5767e0c6288ec51505f438bd70efce
Taejoon Byun <taejoon@umn.edu>
2018-01-20 15:48:15 -0600

    modified a script for testing

--- test/show-segfault-cases.sh
+++ test/show-segfault-cases.sh
@@ -1,2 +1,6 @@
 find ./microwave_auto-gcc/bintraces/ -type f -size 0b -exec ls {} \;
 
+find ./microwave_auto-clang/bintraces/ -type f -size 0b -exec ls {} \;
+
+find ./infusion-gcc/bintraces/ -type f -size 0b -exec ls {} \;
+
