580e209a646bd1d56db35917978695e754697da2
Taejoon Byun <taejoon@umn.edu>
2018-01-31 09:40:41 -0600

    .

--- test/run_buggies.sh
+++ test/run_buggies.sh
@@ -2,9 +2,26 @@ BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 ENDL='\033[0m'
 tmp_out="tmp.txt"
+BUGGIES="$(pwd)/buggies"
 
-for crash_kind in $(ls "buggies"); do
-  dir="$(pwd)/buggies/$crash_kind/"
+function is_tarfile() {
+  [ $(echo $1 | rev | cut -c-6 | rev) == "tar.gz" ] && echo 0 || echo -1
+}
+
+# untar all
+cd $BUGGIES
+for f in $(ls $BUGGIES); do
+  if $(is_tarfile $f); then
+    tar -xzf "$BUGGIES/$f"
+  fi
+done
+cd ../
+
+for crash_kind in $(ls $BUGGIES); do
+  if $(is_tarfile $BUGGIES/$crash_kind); then
+    continue
+  fi
+  dir="$BUGGIES/$crash_kind"
   test="$dir/tc.csv"
   echo -e "${BLUE}Running crashing mutants in setting $GREEN$crash_kind$ENDL"
   for f in $(ls $dir); do
@@ -15,4 +32,4 @@ for crash_kind in $(ls "buggies"); do
   done
 done
 
-rm $tmp_out
+[ -f $tmp_out ] && rm $tmp_out
